42cda088b16d63becfd11dc6660cf87c2d20b9c0
Atakan <104718203+Week-End-Developer@users.noreply.github.com>
2024-07-08 15:12:22 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,6 @@ export class AppComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     if (true == true) {
-    
     }
   }
 
