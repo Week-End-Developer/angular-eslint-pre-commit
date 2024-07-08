ceed6e938879aca0d4b47bdb6757223b71431ac3
Atakan <104718203+Week-End-Developer@users.noreply.github.com>
2024-07-08 15:04:41 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = ' angular-eslint';
-  
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    if (true == true) {
+    
+    }
+  }
+
 }
